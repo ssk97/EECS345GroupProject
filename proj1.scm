e245b0f86d1dec95288d45fcdd0b50c0b3e576ed
Steven <ssk97@case.edu>
2016-02-10 16:22:55 -0500

    added math and while

--- proj1.scm
+++ proj1.scm
@@ -20,20 +20,15 @@
             (else (cons (car state) (removeVar varname (cdr state)))))))
 (define setVar
     (lambda (info state)
-        (addVar info (removeVar (car info) state))))
+        (cond
+            ((eq? (car info) (caar state)) (cons (cons (car info) (Mvalue (cadr info) state)) (cdr state)))
+            (else (cons (car state) (setVar info (cdr state)))))))
 (define findVar
     (lambda (varname state)
         (cond
             ((eq? varname (caar state)) (cdar state))
             (else (findVar varname (cdr state))))))
 ;Primary doing stuff
-(define evalBool
-    (lambda (condition statements state)
-        (if (Mboolean condition state)
-            (Mstate (car statements) state)
-            (if (pair? (cdr statements))
-                (Mstate (cadr statements) state)
-                state))))
 (define outputNice ;converts #t and #f to 'true and 'false respectively
     (lambda (a)
         (cond
@@ -44,41 +39,69 @@
     (lambda (parsetree state)
         (cond
             ((number? state) state)
-            ((boolean? state) (outputNice state))
+            ((boolean? state) (outputNice state));if state is a single value, immediately return
             (else (interpret (cdr parsetree) (Mstate (car parsetree) state))))))
     
+(define operator car)
+(define operand1 cadr)
+(define operand2 caddr)
+
 (define Mstate
     (lambda (statement state)
         (cond
-            ((eq? (car statement) 'return) (Mvalue (cadr statement) state));this replaces state with a value
+            ((eq? (operator statement) 'return) (Mvalue (cadr statement) state));this replaces state with a value
                                                                            ;and ends execution immediately
-            ((eq? (car statement) 'var) (addVar (cdr statement) state))
-            ((eq? (car statement) '=) (setVar (cdr statement) state))
-            ((eq? (car statement) 'if) (evalBool (cadr statement) (cddr statement) state))
+            ((eq? (operator statement) 'var) (addVar (cdr statement) state))
+            ((eq? (operator statement) '=) (setVar (cdr statement) state))
+            ((eq? (operator statement) 'if) (Mstate_if (cadr statement) (cddr statement) state)) ;cddr can have 1 or 2 statements in it: if 2 then it has an 'else' case.
+            ((eq? (operator statement) 'while) (Mstate_while (operand1 statement) (operand2 statement) state))
             (else state)
 )))
+
 (define Mboolean
     (lambda (statement state)
         (cond
             ((eq? statement 'true) #t)
             ((eq? statement 'false) #f)
-            ((eq? (car statement) '==) (eq? (Mvalue (cadr statement) state) (Mvalue (caddr statement) state)))
-            ((eq? (car statement) '!=) (not (eq? (Mvalue (cadr statement) state) (Mvalue (caddr statement) state))))
-            ((eq? (car statement) '>) (> (Mvalue (cadr statement) state) (Mvalue (caddr statement) state)))
-            ((eq? (car statement) '<) (< (Mvalue (cadr statement) state) (Mvalue (caddr statement) state)))
-            ((eq? (car statement) '>=) (>= (Mvalue (cadr statement) state) (Mvalue (caddr statement) state)))
-            ((eq? (car statement) '<=) (<= (Mvalue (cadr statement) state) (Mvalue (caddr statement) state)))
+            ((eq? (operator statement) '==) (eq? (Mvalue (operand1 statement) state) (Mvalue (operand2 statement) state)))
+            ((eq? (operator statement) '!=) (not (eq? (Mvalue (operand1 statement) state) (Mvalue (operand2 statement) state))))
+            ((eq? (operator statement) '>) (> (Mvalue (operand1 statement) state) (Mvalue (operand2 statement) state)))
+            ((eq? (operator statement) '<) (< (Mvalue (operand1 statement) state) (Mvalue (operand2 statement) state)))
+            ((eq? (operator statement) '>=) (>= (Mvalue (operand1 statement) state) (Mvalue (operand2 statement) state)))
+            ((eq? (operator statement) '<=) (<= (Mvalue (operand1 statement) state) (Mvalue (operand2 statement) state)))
 )))
-            
+
 (define Mvalue
-    (lambda (statement state)
+    (lambda (expr state)
         (cond
-            ((number? statement) statement)
-            ((eq? statement 'true) #t)
-            ((eq? statement 'false) #f)
-            ((symbol? statement) (findVar statement state));variable
-            (else (Mboolean statement state))
+            ((number? expr) expr)
+            ((eq? expr 'true) #t)
+            ((eq? expr 'false) #f)
+            ((symbol? expr) (findVar expr state));variable
+            ((eq? (operator expr) '+) (+ (Mvalue (operand1 expr) state) (Mvalue (operand2 expr) state)))
+            ((eq? (operator expr) '*) (* (Mvalue (operand1 expr) state) (Mvalue (operand2 expr) state)))
+            ((eq? (operator expr) '/) (quotient (Mvalue (operand1 expr) state) (Mvalue (operand2 expr) state)))
+            ((eq? (operator expr) '-) (if (eq? (length expr) 3)
+                (- (Mvalue (operand1 expr) state) (Mvalue (operand2 expr) state))
+                (- (Mvalue (operand1 expr) state))));unary - operator
+            ((eq? (operator expr) '%) (remainder (Mvalue (operand1 expr) state) (Mvalue (operand2 expr) state)))
+            (else (Mboolean expr state))
 )))
+
+(define Mstate_while
+    (lambda (condition statement state)
+        (if (Mboolean condition state)
+            (Mstate_while condition statement (Mstate statement state))
+            state)))
+
+(define Mstate_if
+    (lambda (condition statements state)
+        (if (Mboolean condition state)
+            (Mstate (car statements) state)
+            (if (pair? (cdr statements))
+                (Mstate (cadr statements) state)
+                state))))
+
 
 #|
 ((var x)
