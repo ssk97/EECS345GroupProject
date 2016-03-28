107fcddd4221c1d032b1e65920e574cf1440882a
Steven <ssk97@case.edu>
2016-03-28 15:36:38 -0400

    boxed everything, renamed to proj3

--- proj3.scm
+++ proj3.scm
@@ -37,27 +37,29 @@
      ((null? substate) #f)
      ((eq? varname (caar substate)) #t)
      (else (varExists_sub varname (cdr substate))))))
-;Adds a variable named varname with value to substate.  Errors if it already exists
+;Returns substate modified to have a new variable named varname in it
+;Initially sets varname to value.  Errors if it already exists
 (define addVar_sub
   (lambda (varname value substate)
     (if (varExists_sub varname substate)
 	(error "Variable declared multiple times.")
-        (cons (cons varname value) substate))))
-;Returns substate modified so that the entry for varname is set to value.
+        (cons (cons varname (box value)) substate))))
+;modifies the value of the box in substate
+;returns void or errors out
 (define setVar_sub
     (lambda (varname value substate)
 	(cond
 	 ((null? substate) (error "Variable assigned before declared- setVar_sub"))
-	 ((eq? varname (caar substate)) (cons (cons varname value) (cdr substate)))
-	 (else (cons (car substate) (setVar_sub varname value (cdr substate)))))))
+	 ((eq? varname (caar substate)) (set-box! (cdar substate) value))
+	 (else (setVar_sub varname value (cdr substate))))))
 ;Returns the value associated with varname in substate
 (define findVar_sub
     (lambda (varname substate)
 	(cond
 	 ((null? substate) (error "Variable used before declared- findVar_sub"));We've gone too far, there are no substates left
 	 ((not (eq? varname (caar substate))) (findVar_sub varname (cdr substate))) ;Didn't find it, iterate
-	 ((null? (cdar substate)) (error "Variable used before assigned."))
-	 (else (cdar substate)))))
+	 ((null? (unbox (cdar substate))) (error "Variable used before assigned."))
+	 (else (unbox (cdar substate))))))
      
 ;functions that work across entire state
 ;Check if a variable with varname as a name exists in the state at all
@@ -72,14 +74,15 @@
 (define addVar
     (lambda (varname value state)
         (cons (addVar_sub varname value (car state)) (cdr state))))
-;returns state modified so that varname is set to value
+;modifies the boxes such that varname is set to value
 ;Takes a name, value, and state to modify
+;returns void on success, errors out on failure
 (define setVar
     (lambda (varname value state)
         (cond
             ((null? state) (error "Variable used before declared- setVar"))
-            ((varExists_sub varname (car state)) (cons (setVar_sub varname value (car state)) (cdr state)))
-            (else (cons (car state) (setVar varname value (cdr state)))))))
+            ((varExists_sub varname (car state)) ((setVar_sub varname value (car state))))
+            (else (setVar varname value (cdr state))))))
 ;returns the variable varname
 (define findVar
     (lambda (varname state)
